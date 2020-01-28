e3898da25193d09ac47ea4a633abb203b3de6176
Dilan Morar <DMorar@spartaglobal.com>
2020-01-28 16:15:03 +0000

    terraform for nodejs app

---
+++ scripts/init_scripts.sh.tpl
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd home/ubuntu/appjs/app
+sudo npm install
+sudo npm start
+exit
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "Name" {
+  type    = string
+  default = "Eng48-dilan-app"
+}
+
+variable "ami-id" {
+  type = string
+  default = "ami-05ab60c15ddd15a05"
+}
